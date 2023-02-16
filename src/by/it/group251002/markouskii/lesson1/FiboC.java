851ba3e7685a9ff39887c8e4b6d2243e5e042f4e
TheThing <ilia5556523ilia@mail.ru>
2023-02-16 22:22:44 +0300

    Lesson 1 C(time wall)

--- src/by/it/group251002/markouskii/lesson1/FiboC.java
+++ src/by/it/group251002/markouskii/lesson1/FiboC.java
@@ -5,7 +5,7 @@ package by.it.group251002.markouskii.lesson1;
  * необходимо найти остаток от деления n-го числа Фибоначчи на m.
  * время расчета должно быть не более 2 секунд
  */
-
+import java.util.ArrayList;
 public class FiboC {
 
     private long startTime = System.currentTimeMillis();
@@ -25,7 +25,24 @@ public class FiboC {
         //Решение сложно найти интуитивно
         //возможно потребуется дополнительный поиск информации
         //см. период Пизано
-        return 0L;
+        long period=2,remain1;
+        ArrayList<Long> remains = new ArrayList<>();
+        long[] fib = {0, 1, 1};
+        remains.add((long)0);
+        remains.add((long)1);
+
+        do {
+            remain1=(int)fib[2]%m;
+            remains.add(remain1);
+            period++;
+            fib[0]=fib[1];
+            fib[1]=fib[2];
+            fib[2]=fib[0]+fib[1];
+
+
+        } while (remain1!=0 && period<n-1);
+
+        return remains.get((int)(n%period));
     }
 
 
