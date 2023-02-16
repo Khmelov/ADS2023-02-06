4fc1d8bc5aaebe694a61ecb01f3c9494f54f6a63
Alexandr Voytov <voytov1704@gmail.com>
2023-02-16 23:44:37 +0300

    first two tasks done

---
+++ src/by/it/group251001/voytov/lesson01/FiboC.java
@@ -0,0 +1,36 @@
+package by.it.group251001.voytov.lesson01;
+
+import java.math.BigInteger;
+/*
+ * Даны целые числа 1<=n<=1E18 и 2<=m<=1E5,
+ * необходимо найти остаток от деления n-го числа Фибоначчи на m.
+ * время расчета должно быть не более 2 секунд
+ */
+
+public class FiboC {
+
+    private final long startTime = System.currentTimeMillis();
+
+    private long time() {
+        return System.currentTimeMillis() - startTime;
+    }
+
+    public static void main(String[] args) {
+        FiboC fibo = new FiboC();
+        int n = 10;
+        int m = 2;
+        System.out.printf("fasterC(%d)=%d \n\t time=%d \n\n", n, fibo.fasterC(n, m), fibo.time());
+    }
+
+    long fasterC(long n, int m) {
+
+
+        return 0;
+    }
+
+}
+
+
+
+
+
