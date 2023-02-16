0b9ecd6ee417ba6b23d38155dd034497fd7c41ac
Alexandr Voytov <voytov1704@gmail.com>
2023-02-16 23:46:41 +0300

    test

--- src/by/it/group251001/voytov/lesson01/FiboC.java
+++ src/by/it/group251001/voytov/lesson01/FiboC.java
@@ -18,6 +18,7 @@ public class FiboC {
     public static void main(String[] args) {
         FiboC fibo = new FiboC();
         int n = 10;
+        int c = 20;
         int m = 2;
         System.out.printf("fasterC(%d)=%d \n\t time=%d \n\n", n, fibo.fasterC(n, m), fibo.time());
     }
