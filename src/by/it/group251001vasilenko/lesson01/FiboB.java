28a69fbe6a34c0173aca6798e7117237d1aef044
Vasilenko Ivan <vanya.vasilenko2005@gmail.com>
2023-02-17 21:21:00 +0300

    urok1

--- src/by/it/group251001vasilenko/lesson01/FiboB.java
+++ src/by/it/group251001vasilenko/lesson01/FiboB.java
@@ -20,13 +20,31 @@ public class FiboB {
         //вычисление чисел простым быстрым методом
         FiboB fibo = new FiboB();
         int n = 55555;
+
         System.out.printf("fastB(%d)=%d \n\t time=%d \n\n", n, fibo.fastB(n), fibo.time());
     }
 
     BigInteger fastB(Integer n) {
-        //здесь нужно реализовать вариант с временем O(n) и памятью O(n)
-        return BigInteger.ZERO;
+
+        BigInteger[] fibonacciArr = new BigInteger[n + 1];
+
+        return countFibonacci(n, fibonacciArr);
     }
 
+    private static BigInteger countFibonacci(int n, BigInteger[] fibonacciArr) {
+
+        if (n < 2) {
+            return BigInteger.valueOf(n);
+        }
+
+        fibonacciArr[0] = BigInteger.ZERO;
+        fibonacciArr[1] = BigInteger.ONE;
+
+        for (int i = 2; i < fibonacciArr.length; i++) {
+            fibonacciArr[i] = fibonacciArr[i - 1].add(fibonacciArr[i - 2]);
+        }
+
+        return fibonacciArr[n];
+    }
 }
 
