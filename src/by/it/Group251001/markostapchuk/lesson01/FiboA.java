21d114598641208a830cc1d02dd551c1fcec9e28
Mark <markostapchukwork@gmail.com>
2023-02-17 20:32:42 +0300

    FiboA done

--- src/by/it/Group251001/markostapchuk/lesson01/FiboA.java
+++ src/by/it/Group251001/markostapchuk/lesson01/FiboA.java
@@ -32,7 +32,8 @@ public class FiboA {
         //здесь простейший вариант, в котором код совпадает
         //с математическим определением чисел Фибоначчи
         //время O(2^n)
-        return 0;
+        if (n <= 1) return n;
+        return calc(n - 1) + calc(n - 2);
     }
 
 
@@ -41,9 +42,9 @@ public class FiboA {
         //здесь нужно реализовать вариант без ограничения на размер числа,
         //в котором код совпадает с математическим определением чисел Фибоначчи
         //время O(2^n)
+        if (n <= 1) return BigInteger.valueOf(n);
+        return slowA(n - 1).add(slowA(n - 2));
 
-
-        return BigInteger.ZERO;
     }
 
 
