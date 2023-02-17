d03353bc84f461977ff91c7096edc17f0aef39b4
Егор <egorkaquell1steam@gmaul.com>
2023-02-17 22:05:25 +0300

    Lesson A + B

---
+++ src/by/it/group251002/korti/lesson01/FiboC.java
@@ -0,0 +1,40 @@
+package by.it.group251002.korti.lesson01;
+
+/*
+ * Даны целые числа 1<=n<=1E18 и 2<=m<=1E5,
+ * необходимо найти остаток от деления n-го числа Фибоначчи на m.
+ * время расчета должно быть не более 2 секунд
+ */
+
+public class FiboC {
+
+    private long startTime = System.currentTimeMillis();
+
+    private long time() {
+        return System.currentTimeMillis() - startTime;
+    }
+
+    public static void main(String[] args) {
+        FiboC fibo = new FiboC();
+        int n = 10;
+        int m = 2;
+        System.out.printf("fasterC(%d)=%d \n\t time=%d \n\n", n, fibo.fasterC(n, m), fibo.time());
+    }
+
+    long fasterC(long n, int m) {
+        //Решение сложно найти интуитивно
+        //возможно потребуется дополнительный поиск информации
+        //см. период Пизано
+        int i = 2;
+        long[] nums = {0, 1};
+        do {
+            nums[i] = ((nums[i-2] + nums[i-1]) % m);
+            i++;
+        } while (!(nums[i-2] == 0 && nums[i-1] == 1) && i < n );
+
+        return nums[(int) (n % (i - 2))];
+    }
+
+
+}
+
