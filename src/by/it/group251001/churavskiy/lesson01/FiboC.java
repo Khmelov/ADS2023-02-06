1c156732fe6bee1b34c9aa9a874cb3da3a3f8e93
37529 <375292816898@yandex.com>
2023-02-26 18:47:35 +0300

    lesson1TaskA completed lesson1TaskB completed lesson1TaskC completed

--- src/by/it/group251001/churavskiy/lesson01/FiboC.java
+++ src/by/it/group251001/churavskiy/lesson01/FiboC.java
@@ -6,7 +6,9 @@ package by.it.group251001.churavskiy.lesson01;
  * время расчета должно быть не более 2 секунд
  */
 
+import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class FiboC {
 
@@ -17,29 +19,26 @@ public class FiboC {
     }
 
     public static void main(String[] args) {
-        FiboC fibo = new FiboC();
-        int n = 10;
-        int m = 2;
-        System.out.printf("fasterC(%d)=%d \n\t time=%d \n\n", n, fibo.fasterC(n, m), fibo.time());
+            FiboC fibo = new FiboC();
+            int n = 10;
+            int m = 5555;
+            System.out.printf("fasterC(%d, %d)=%d \n\t time=%d \n\n", n, m, fibo.fasterC(n, m), fibo.time());
     }
 
     long fasterC(long n, int m) {
         //Решение сложно найти интуитивно
         //возможно потребуется дополнительный поиск информации
         //см. период Пизано
-
-        int[] mas_fib = {0, 1};
-        int currentFibNumber = 0;
-
-        ArrayList<Integer> mas_sequence = new ArrayList<Integer>();
-
-        for (int i = 0; i < n; i++) {
-            currentFibNumber = mas_fib[0] + mas_fib[1];
-            mas_fib[0] = mas_fib[1];
-            mas_fib[1] = currentFibNumber;
-        }
-
-        return 0L;
+        ArrayList<Integer> fibMas = new ArrayList<Integer>();
+        fibMas.add(0);
+        fibMas.add(1);
+        int period = 0;
+        do {
+            fibMas.add( ( fibMas.get(fibMas.size() - 1) + fibMas.get(fibMas.size() - 2) ) % m );
+            period++;
+        } while (!( (fibMas.get(fibMas.size() - 2) == 0) && (fibMas.get(fibMas.size() - 1) == 1) ) );
+        int index = (int)((n + 1) % period) - 1;
+        return fibMas.get(index);
     }
 
 
