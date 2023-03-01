8d300869096cf5a72a102ff0211f0dec89454753
Alexander Khmelov <akhmelev@gmail.com>
2023-03-01 11:20:49 +0300

    ADD: lesson 03. Huffman.

---
+++ src/by/it/a_khmelev/lesson03/B_Huffman.java
@@ -0,0 +1,70 @@
+package by.it.a_khmelev.lesson03;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+// Lesson 3. B_Huffman.
+// Восстановите строку по её коду и беспрефиксному коду символов.
+
+// В первой строке входного файла заданы два целых числа
+// kk и ll через пробел — количество различных букв, встречающихся в строке,
+// и размер получившейся закодированной строки, соответственно.
+//
+// В следующих kk строках записаны коды букв в формате "letter: code".
+// Ни один код не является префиксом другого.
+// Буквы могут быть перечислены в любом порядке.
+// В качестве букв могут встречаться лишь строчные буквы латинского алфавита;
+// каждая из этих букв встречается в строке хотя бы один раз.
+// Наконец, в последней строке записана закодированная строка.
+// Исходная строка и коды всех букв непусты.
+// Заданный код таков, что закодированная строка имеет минимальный возможный размер.
+//
+//        Sample Input 1:
+//        1 1
+//        a: 0
+//        0
+
+//        Sample Output 1:
+//        a
+
+
+//        Sample Input 2:
+//        4 14
+//        a: 0
+//        b: 10
+//        c: 110
+//        d: 111
+//        01001100100111
+
+//        Sample Output 2:
+//        abacabad
+
+public class B_Huffman {
+
+    String decode(File file) throws FileNotFoundException {
+        StringBuilder result=new StringBuilder();
+        //прочитаем строку для кодирования из тестового файла
+        Scanner scanner = new Scanner(file);
+        Integer count = scanner.nextInt();
+        Integer length = scanner.nextInt();
+        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! НАЧАЛО ЗАДАЧИ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+        //тут запишите ваше решение
+
+
+
+
+        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! КОНЕЦ ЗАДАЧИ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+        return result.toString(); //01001100100111
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String root = System.getProperty("user.dir") + "/src/";
+        File f = new File(root + "by/it/a_khmelev/lesson03/encodeHuffman.txt");
+        B_Huffman instance = new B_Huffman();
+        String result = instance.decode(f);
+        System.out.println(result);
+    }
+
+
+}
