d4808ec1527c4d41ec75414bf1a95ba6caf4a573
milka <milka.korotkaya@gmail.com>
2023-03-01 19:45:18 +0300

    Lesson1 A,B,C

--- src/by/it/group251001/korotkaya/lesson01/FiboC.java
+++ src/by/it/group251001/korotkaya/lesson01/FiboC.java
@@ -1,11 +1,11 @@
-package by.it.korotkaya.lesson01;
+package by.it.group251001.korotkaya.lesson01;
 
 /*
  * Даны целые числа 1<=n<=1E18 и 2<=m<=1E5,
  * необходимо найти остаток от деления n-го числа Фибоначчи на m.
  * время расчета должно быть не более 2 секунд
  */
-import java.math.BigInteger;
+
 public class FiboC {
 
     private long startTime = System.currentTimeMillis();
