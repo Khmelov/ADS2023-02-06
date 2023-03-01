55f86f68393266ffc5d02106939f51b0e452cddd
sazonovItas <125500423+sazonovItas@users.noreply.github.com>
2023-03-01 12:30:08 +0300

    Update FiboC.java

--- src/by/it/group251002/sazonov/lesson01/FiboC.java
+++ src/by/it/group251002/sazonov/lesson01/FiboC.java
@@ -18,8 +18,8 @@ public class FiboC {
 
     public static void main(String[] args) {
         FiboC fibo = new FiboC();
-        int n = 2;
-        int m = 10;
+        int n = 10;
+        int m = 2;
         System.out.printf("fasterC(%d)=%d \n\t time=%d \n\n", n, fibo.fasterC(n, m), fibo.time());
     }
 
