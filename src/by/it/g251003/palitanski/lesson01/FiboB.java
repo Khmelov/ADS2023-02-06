dd4669119f0b2950fcd4c816cde311acf15ee966
StarkSpecter <ldrstark@gmail.com>
2023-03-01 13:42:16 +0300

    l1ab

---
+++ src/by/it/g251003/palitanski/lesson01/FiboB.java
@@ -0,0 +1,37 @@
+package by.it.g251003.palitanski.lesson01;
+
+import java.math.BigInteger;
+
+/*
+ * Вам необходимо выполнить способ вычисления чисел Фибоначчи с вспомогательным массивом
+ * без ограничений на размер результата (BigInteger)
+ */
+
+public class FiboB {
+
+    private long startTime = System.currentTimeMillis();
+
+    private long time() {
+        return System.currentTimeMillis() - startTime;
+    }
+
+    public static void main(String[] args) {
+
+        //вычисление чисел простым быстрым методом
+        FiboB fibo = new FiboB();
+        int n = 55555;
+        System.out.printf("fastB(%d)=%d \n\t time=%d \n\n", n, fibo.fastB(n), fibo.time());
+    }
+
+    BigInteger fastB(Integer n) {
+        //здесь нужно реализовать вариант с временем O(n) и памятью O(n)
+        BigInteger[] array = new BigInteger[n];
+        array[0] = BigInteger.ONE;
+        array[1] = BigInteger.ONE;
+        for (int i = 2; i < n; i++)
+            array[i] = array[i - 1].add(array[i - 2]);
+        return array[n-1];
+    }
+
+}
+
