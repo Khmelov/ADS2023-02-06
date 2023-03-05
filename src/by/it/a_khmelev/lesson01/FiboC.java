715216f33a3d1c5dcdd813e10f0643be3e89aa6f
Maksim Ustsinovich <maksimustsinovich@outlook.com>
2023-03-05 17:09:43 +0300

    fix

--- src/by/it/a_khmelev/lesson01/FiboC.java
+++ src/by/it/a_khmelev/lesson01/FiboC.java
@@ -6,9 +6,6 @@ package by.it.a_khmelev.lesson01;
  * время расчета должно быть не более 2 секунд
  */
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class FiboC {
 
     private long startTime = System.currentTimeMillis();
@@ -19,8 +16,8 @@ public class FiboC {
 
     public static void main(String[] args) {
         FiboC fibo = new FiboC();
-        int n = 55555;
-        int m = 1000;
+        int n = 10;
+        int m = 2;
         System.out.printf("fasterC(%d)=%d \n\t time=%d \n\n", n, fibo.fasterC(n, m), fibo.time());
     }
 
@@ -28,16 +25,9 @@ public class FiboC {
         //Решение сложно найти интуитивно
         //возможно потребуется дополнительный поиск информации
         //см. период Пизано
-        List<Long> o = new ArrayList<>();
-        o.add(0L);
-        o.add(1L);
-        int i = 2;
-        while (!(o.get(i - 2) == 0 && o.get(i - 1) == 1) || i <= 2) {
-            o.add((o.get(i - 2) + o.get(i - 1)) % m);
-            i++;
-        }
-        return o.get((int) (n % (i - 2)));
+        return 0L;
     }
 
 
 }
+
