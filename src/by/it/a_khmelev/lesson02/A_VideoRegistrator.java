01081cf141f177c276f52df20ad9e28e77883a22
Mathew-bal <kmk080405@gmail.com>
2023-03-12 19:34:31 +0300

    Lesson1

--- src/by/it/a_khmelev/lesson02/A_VideoRegistrator.java
+++ src/by/it/a_khmelev/lesson02/A_VideoRegistrator.java
@@ -1,7 +1,6 @@
 package by.it.a_khmelev.lesson02;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 /*
 Даны события events
@@ -18,7 +17,6 @@ public class A_VideoRegistrator {
         double[] events=new double[]{1, 1.1, 1.6, 2.2, 2.4, 2.7, 3.9, 8.1, 9.1, 5.5, 3.7};
         List<Double> starts=instance.calcStartTimes(events,1); //рассчитаем моменты старта, с длинной сеанса 1
         System.out.println(starts);                            //покажем моменты старта
-
     }
     //модификаторы доступа опущены для возможности тестирования
     List<Double> calcStartTimes(double[] events, double workDuration){
