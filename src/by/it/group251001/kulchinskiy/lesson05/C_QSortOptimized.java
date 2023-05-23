0fb2fc69b6b61085a6b4dbffe93e87801549f899
amy1181996 <amy1181996@gmail.com>
2023-05-23 04:31:30 +0300

    fixed qsortoptimized

--- src/by/it/group251001/kulchinskiy/lesson05/C_QSortOptimized.java
+++ src/by/it/group251001/kulchinskiy/lesson05/C_QSortOptimized.java
@@ -6,6 +6,12 @@ import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class C_QSortOptimized {
 
     private class Segment implements Comparable<Segment> {
@@ -19,59 +25,8 @@ public class C_QSortOptimized {
 
         @Override
         public int compareTo(Segment o) {
-            if (this.start < o.start)
-                return -1;
-            else if (this.start > o.start)
-                return 1;
-            else
-                return 0;
+            return Integer.compare(this.start, o.start);
         }
-    }
-
-    private int binarySearch(Segment[] segments, int point) {
-        int left = 0;
-        int right = segments.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (point < segments[mid].start)
-                right = mid - 1;
-            else if (point > segments[mid].stop)
-                left = mid + 1;
-            else
-                return mid;
-        }
-
-        return left;
-    }
-
-    private void quickSort(Segment[] segments, int low, int high) {
-        if (high <= low)
-            return;
-
-        int lt = low;
-        int i = low + 1;
-        int gt = high;
-        Segment pivot = segments[low];
-
-        while (i <= gt) {
-            int cmp = segments[i].compareTo(pivot);
-            if (cmp < 0)
-                swap(segments, lt++, i++);
-            else if (cmp > 0)
-                swap(segments, i, gt--);
-            else
-                i++;
-        }
-
-        quickSort(segments, low, lt - 1);
-        quickSort(segments, gt + 1, high);
-    }
-
-    private void swap(Segment[] segments, int i, int j) {
-        Segment temp = segments[i];
-        segments[i] = segments[j];
-        segments[j] = temp;
     }
 
     int[] getAccessory2(InputStream stream) throws FileNotFoundException {
@@ -86,12 +41,30 @@ public class C_QSortOptimized {
             segments[i] = new Segment(scanner.nextInt(), scanner.nextInt());
         }
 
-        quickSort(segments, 0, n - 1);
-
         for (int i = 0; i < m; i++) {
             points[i] = scanner.nextInt();
-            int index = binarySearch(segments, points[i]);
-            result[i] = (index >= 0 && index < segments.length && points[i] <= segments[index].stop) ? 1 : 0;
+        }
+
+        Arrays.sort(segments);
+
+        for (int i = 0; i < m; i++) {
+            int point = points[i];
+            int leftIndex = 0;
+            int rightIndex = n - 1;
+
+            while (leftIndex <= rightIndex) {
+                int middleIndex = leftIndex + (rightIndex - leftIndex) / 2;
+                Segment segment = segments[middleIndex];
+
+                if (point >= segment.start && point <= segment.stop) {
+                    result[i] = 1;
+                    break;
+                } else if (point < segment.start) {
+                    rightIndex = middleIndex - 1;
+                } else {
+                    leftIndex = middleIndex + 1;
+                }
+            }
         }
 
         return result;
