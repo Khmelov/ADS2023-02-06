4a24be7fd9a6f797cf062c19f48bcff22820ef4d
Mathew-bal <kmk080405@gmail.com>
2023-05-31 12:57:04 +0300

    lessons 8 finished

--- src/by/it/group251001/karpekov/lesson08/C_Stairs.java
+++ src/by/it/group251001/karpekov/lesson08/C_Stairs.java
@@ -42,13 +42,21 @@ public class C_Stairs {
             stairs[i]=scanner.nextInt();
         }
         //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        int result = 0;
 
 
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = stairs[0];
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.max(dp[i-1] + stairs[i-1], dp[i-2] + stairs[i-1]);
+        }
+
+        return dp[n];
 
 
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        return result;
+
     }
 
 
