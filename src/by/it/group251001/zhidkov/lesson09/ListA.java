5ab3d6b4d6aafbf15126733432abae9d09f8715e
Arsen1 <arsflyer57@gmail.com>
2023-09-28 16:14:10 +0300

    Lesson9

--- src/by/it/group251001/zhidkov/lesson09/ListA.java
+++ src/by/it/group251001/zhidkov/lesson09/ListA.java
@@ -6,7 +6,14 @@ import java.util.List;
 import java.util.ListIterator;
 
 public class ListA<E> implements List<E> {
+    private Object[] ListElements; // Массив для хранения элементов списка
+    private int Lsize;          // Текущий размер списка
 
+    // Конструктор для создания пустого списка
+    public ListA() {
+        ListElements = new Object[10]; // Начальный размер списка
+        Lsize = 0;
+    }
     //Создайте аналог списка БЕЗ использования других классов СТАНДАРТНОЙ БИБЛИОТЕКИ
 
     /////////////////////////////////////////////////////////////////////////
@@ -16,22 +23,55 @@ public class ListA<E> implements List<E> {
     /////////////////////////////////////////////////////////////////////////
     @Override
     public String toString() {
-        return "";
+            StringBuilder result = new StringBuilder();
+            result.append("[");
+            for (int i = 0; i < Lsize; i++)
+            {
+                result.append(ListElements[i]);
+                if (i < Lsize - 1)
+                   result.append(", ");
+            }
+            result.append("]");
+        return result.toString();
     }
 
-    @Override
+     @Override
     public boolean add(E e) {
-        return false;
+        if (Lsize == ListElements.length)
+        {
+            int newCapacity = Lsize * 2;
+            //Умножаю на 2, чтобы уменьшить частоту необходимости увеличения массива
+            Object[] newListEl = new Object[newCapacity];
+            for (int i = 0; i < Lsize; i++)
+            {
+                newListEl[i] = ListElements[i];
+            }
+            ListElements = newListEl;
+        }
+        ListElements[Lsize] = e;
+        Lsize++;
+        return true;
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        if (index < 0 || index >= Lsize)
+        {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        E removedElement = (E) ListElements[index];
+
+        for (int i = index; i < Lsize; i++)
+        {
+            ListElements[i] = ListElements[i + 1];
+        }
+        Lsize--;
+        return removedElement;
     }
 
     @Override
     public int size() {
-        return 0;
+        return Lsize;
     }
 
     /////////////////////////////////////////////////////////////////////////
@@ -42,49 +82,132 @@ public class ListA<E> implements List<E> {
 
     @Override
     public void add(int index, E element) {
+        int newCapacity = Lsize + 1;
+        Object[] newListEl = new Object[newCapacity];
 
+        if (ListElements.length == Lsize)
+        {
+            //Умножаю на 2, чтобы уменьшить частоту необходимости увеличения массива
+            for (int i = 0; i < Lsize; i++)
+            {
+                newListEl[i] = ListElements[i];
+            }
+            ListElements = newListEl;
+        }
+        // Копируем элементы до индекса index
+        for (int i = 0; i < index; i++) {
+            newListEl[i] = ListElements[i];
+        }
+
+// Вставляем новый элемент в позицию index
+        newListEl[index] = element;
+
+// Копируем остальные элементы после индекса index
+        for (int i = index; i < Lsize; i++) {
+            newListEl[i + 1] = ListElements[i];
+        }
+
+// Присваиваем newData в data, если это необходимо
+        ListElements = newListEl;
+        Lsize++;
     }
 
     @Override
     public boolean remove(Object o) {
-        return false;
+        int index = indexOf(o);
+        if (index > -1)
+        {
+            remove(index);
+        }
+        return (index > -1);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        if (index < 0 || index >= Lsize)
+        {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        E prevEl = (E)ListElements[index];
+        ListElements[index] = element;
+        return prevEl;
     }
 
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return (Lsize == 0);
     }
 
 
     @Override
     public void clear() {
-
+        for (int i = 0; i < Lsize; i++) {
+            ListElements[i] = null;
+        }
+        Lsize = 0;
     }
 
     @Override
     public int indexOf(Object o) {
-        return 0;
+        if (o == null)
+        {
+            for (int i = 0; i < Lsize; i++)
+            {
+                if (ListElements[i] == null)
+                {
+                    return  i;
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < Lsize; i++)
+            {
+                if (ListElements[i] == o)
+                {
+                    return  i;
+                }
+            }
+        }
+
+        return -1;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return (E)ListElements[index];
     }
 
     @Override
     public boolean contains(Object o) {
-        return false;
+
+        return (indexOf(o) > -1);
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        if (o == null)
+        {
+            for (int i = Lsize - 1; i >= 0; i--)
+            {
+                if (ListElements[i] == null)
+                {
+                    return i;
+                }
+            }
+        }
+        else
+        {
+            for (int i = Lsize - 1; i >= 0; i--)
+            {
+                if (o == ListElements[i])
+                {
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
 
     @Override
