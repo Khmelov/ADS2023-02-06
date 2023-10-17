128db03567d85cd1ea78c92ce162324716917dcd
Arsen1 <arsflyer57@gmail.com>
2023-10-17 21:36:14 +0300

    Lesson10

--- src/by/it/group251001/zhidkov/lesson11/MyHashSet.java
+++ src/by/it/group251001/zhidkov/lesson11/MyHashSet.java
@@ -1,4 +1,72 @@
 package by.it.group251001.zhidkov.lesson11;
 
-public class MyHashSet {
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+public class MyHashSet<E> implements Set<E> {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(E e) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
 }
