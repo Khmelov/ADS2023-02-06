103f9505c9557ca7f7ce32458430050854769d7e
EvgenSavenok <eugen.savenok@gmail.com>
2023-12-20 17:06:46 +0300

    Lesson 13 (A, B, C)

---
+++ src/by/it/group251004/savenok/lesson13/Test_Part2_Lesson13.java
@@ -0,0 +1,41 @@
+package by.it.group251004.savenok.lesson13;
+
+import by.it.HomeWork;
+import org.junit.Test;
+
+@SuppressWarnings("NewClassNamingConvention")
+public class Test_Part2_Lesson13 extends HomeWork {
+
+    @Test
+    public void testGraphA() {
+        run("0 -> 1", true).include("0 1");
+        run("0 -> 1, 1 -> 2", true).include("0 1 2");
+        run("0 -> 2, 1 -> 2, 0 -> 1", true).include("0 1 2");
+
+
+        //Дополните эти тесты СВОИМИ более сложными примерами и проверьте их работоспособность.
+        //Параметр метода run - это ввод. Параметр метода include - это вывод.
+        //Общее число примеров должно быть не менее 20 (сейчас их 8).
+    }
+
+    @Test
+    public void testGraphB() {
+        run("0 -> 1", true).include("no").exclude("yes");
+        run("0 -> 1, 1 -> 2", true).include("no").exclude("yes");
+        run("0 -> 1, 1 -> 2, 2 -> 0", true).include("yes").exclude("no");
+        //Дополните эти тесты СВОИМИ более сложными примерами и проверьте их работоспособность.
+        //Параметр метода run - это ввод. Параметр метода include - это вывод.
+        //Общее число примеров должно быть не менее 12 (сейчас их 3).
+    }
+
+    @Test
+    public void testGraphC() {
+
+
+        //Дополните эти тесты СВОИМИ более сложными примерами и проверьте их работоспособность.
+        //Параметр метода run - это ввод. Параметр метода include - это вывод.
+        //Общее число примеров должно быть не менее 8 (сейчас их 2).
+    }
+
+
+}
