d7f8f565ba992d124917e5f304116b94ca98c010
akhmelev <akhmelev@gmail.com>
2023-12-22 14:11:05 +0300

    FIX: lesson15 (restore)

--- src/by/it/a_khmelev/lesson05/A_QSort.java
+++ src/by/it/a_khmelev/lesson05/A_QSort.java
@@ -1,8 +1,8 @@
 package by.it.a_khmelev.lesson05;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.util.Arrays;
 import java.util.Scanner;
 
 /*
@@ -11,14 +11,18 @@ import java.util.Scanner;
 Известны данные о том, когда каждая из них включалась и выключалась (отрезки работы)
 Известен список событий на площади (время начала каждого события).
 Вам необходимо определить для каждого события сколько камер его записали.
+
 В первой строке задано два целых числа:
     число включений камер (отрезки) 1<=n<=50000
     число событий (точки) 1<=m<=50000.
+
 Следующие n строк содержат по два целых числа ai и bi (ai<=bi) -
 координаты концов отрезков (время работы одной какой-то камеры).
 Последняя строка содержит m целых чисел - координаты точек.
 Все координаты не превышают 10E8 по модулю (!).
+
 Точка считается принадлежащей отрезку, если она находится внутри него или на границе.
+
 Для каждой точки в порядке их появления во вводе выведите,
 скольким отрезкам она принадлежит.
     Sample Input:
@@ -28,6 +32,7 @@ import java.util.Scanner;
     1 6 11
     Sample Output:
     1 0 0
+
 */
 
 public class A_QSort {
@@ -47,8 +52,8 @@ public class A_QSort {
         @Override
         public int compareTo(Segment o) {
             //подумайте, что должен возвращать компаратор отрезков
-            return 0 ;
 
+            return 0;
         }
     }
 
@@ -76,18 +81,7 @@ public class A_QSort {
         }
         //тут реализуйте логику задачи с применением быстрой сортировки
         //в классе отрезка Segment реализуйте нужный для этой задачи компаратор
-        Arrays.sort(segments);
-        for (int i = 0; i < m; i++) {
-            int count = 0;
-            int j = 0;
-            while (j < n && segments[j].start <= points[i]) {
-                if (segments[j].stop >= points[i]) {
-                    count++;
-                }
-                j++;
-            }
-            result[i] = count;
-        }
+
 
 
 
--- src/by/it/a_khmelev/lesson05/C_QSortOptimized.java
+++ src/by/it/a_khmelev/lesson05/C_QSortOptimized.java
@@ -3,20 +3,22 @@ package by.it.a_khmelev.lesson05;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.util.Arrays;
 import java.util.Scanner;
 
 /*
 Видеорегистраторы и площадь 2.
 Условие то же что и в задаче А.
+
         По сравнению с задачей A доработайте алгоритм так, чтобы
         1) он оптимально использовал время и память:
             - за стек отвечает элиминация хвостовой рекурсии,
             - за сам массив отрезков - сортировка на месте
             - рекурсивные вызовы должны проводиться на основе 3-разбиения
+
         2) при поиске подходящих отрезков для точки реализуйте метод бинарного поиска
         для первого отрезка решения, а затем найдите оставшуюся часть решения
         (т.е. отрезков, подходящих для точки, может быть много)
+
     Sample Input:
     2 3
     0 5
@@ -24,6 +26,7 @@ import java.util.Scanner;
     1 6 11
     Sample Output:
     1 0 0
+
 */
 
 
@@ -42,7 +45,7 @@ public class C_QSortOptimized {
         @Override
         public int compareTo(Object o) {
             //подумайте, что должен возвращать компаратор отрезков
-            return   Integer.compare(this.stop, ((Segment) o).stop);
+            return 0;
         }
     }
 
@@ -65,37 +68,13 @@ public class C_QSortOptimized {
             segments[i]=new Segment(scanner.nextInt(),scanner.nextInt());
         }
         //читаем точки
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < n; i++) {
             points[i]=scanner.nextInt();
         }
         //тут реализуйте логику задачи с применением быстрой сортировки
         //в классе отрезка Segment реализуйте нужный для этой задачи компаратор
-        int temp = segments[0].start;
-        for (int i = 0; i < n; i++) {
-            if (segments[i].start < temp) {
-                temp = segments[i].start; // вместо бинарного поиска наименьшее значение правого края, чтобы уменьшить область поиска
-            }
-        }
-        // проходим по всем точкам и ищем отрезки, которые их содержат
-        for (int i = 0; i < m; i++) {
-            int point = points[i];
-            int left = 0;
-            int right = n - 1;
-            if (left == 0 && point < temp) {
-                // точка находится за пределами всех отрезков
-                result[i] = 0;
-            } else {
-                int count = 0;
-                // теперь ищем оставшиеся отрезки, содержащие точку
-                for (int j = left; j < n && segments[j].start <= point; j++) {
-                    if (point <= segments[j].stop) {
-                        count++;
-                    }
-                }
-                result[i] = count;
-            }
 
-        }
+
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
         return result;
     }
@@ -105,9 +84,9 @@ public class C_QSortOptimized {
         String root = System.getProperty("user.dir") + "/src/";
         InputStream stream = new FileInputStream(root + "by/it/a_khmelev/lesson05/dataC.txt");
         C_QSortOptimized instance = new C_QSortOptimized();
-        int[] result = instance.getAccessory2(stream);
-        for (int index : result) {
-            System.out.print(index + " ");
+        int[] result=instance.getAccessory2(stream);
+        for (int index:result){
+            System.out.print(index+" ");
         }
     }
 
